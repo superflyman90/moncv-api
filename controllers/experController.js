35da350c3ec8503d7e8b8111f8a020aa2394ba8a
Manfred PARBATIA <superflyman90@gmail.com>
2022-01-17 08:43:22 +0100

    ajout route et controller pour voir une experience

--- controllers/experController.js
+++ controllers/experController.js
@@ -1,5 +1,7 @@
 import ExperModel from '../models/experModel.js';
 
+
+// Ajouter une expérience
 export const addExper = async (req, res) => {
     const exper = new ExperModel(req.body);
 
@@ -7,7 +9,15 @@ export const addExper = async (req, res) => {
     res.send(exper)
 }
 
-export const getExper = async (req, res) =>{
+// Lire les expériences
+export const getExpers = async (req, res) =>{
     const expers = await ExperModel.find({});
     res.send(expers);
+}
+
+// Lire une expérience
+export const getExper = async (req, res) =>{
+    console.log(req.params.id);
+    const exper = await ExperModel.find({_id: req.params.id});
+    res.send(exper);
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,20 @@
 import { Router } from 'express';
-import { addExper, getExper } from '../controllers/experController.js';
+
+import { addExper, getExpers, getExper } from '../controllers/experController.js';
+
 import { catchErrors } from '../helpers.js';
+
 
 const router = new Router();
 
 router.get('/', (req, res) => {
     res.send('<h1>Hello World !</h1>');
-})
+});
 
 router.post('/exper', catchErrors(addExper));
 
-router.get('/expers', catchErrors(getExper));
+router.get('/exper/:id', catchErrors(getExper));
+
+router.get('/expers', catchErrors(getExpers));
 
 export default router;
